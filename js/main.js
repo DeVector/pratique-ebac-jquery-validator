6a8fc3adf73ebf562b225da17b02790295dce8d8
DeVector <diniz.vito@gmail.com>
2024-01-22 16:09:43 -0300

    Add jquery no projeto

--- js/main.js
+++ js/main.js
@@ -23,6 +23,8 @@ $(document).ready(function () {
 
     $('#telefone').mask('(00) 00000-0000')
     $('#cpf').mask('000.000.000-00')
-    $('#cep').mask('0000-000')
+    $('.cep').mask('0000-000')
+
+    console.log($('.cep'))
 
 });
