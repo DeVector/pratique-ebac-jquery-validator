4ce4bfa9aa996cb266db48949d1883d993f4d66d
DeVector <diniz.vito@gmail.com>
2024-01-22 15:43:28 -0300

    Commit initial

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    
+    $('#form-adress').validate({
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            cpf: {
+                required: true
+            },
+            endCompleto: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        }
+    })
+
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('0000-000')
+
+});
